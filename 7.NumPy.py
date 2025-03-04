3c70824facb155ca22ea374abe9c2916130a0b78
YOGEESH K P <yogeesh793@gmail.com>
2025-03-04 21:27:25 +0530

    Add files via upload

---
+++ 7.NumPy.py
@@ -0,0 +1,55 @@
+# Create a 0-D array with value 42
+import numpy as np
+arr=np.array(42)
+print(arr)
+
+# Create a 0-D array with value 51
+import numpy as an
+anr = an.array(51)
+print(anr)
+
+# Create a 0-D array with value 552
+import numpy as bn
+bnr = bn.array(552)
+print(bnr)
+
+# Create 0-D array with value 533
+import numpy as cn
+cnr=cn.array(533)
+print(cnr)
+
+#Create 0-D array with value 54
+import numpy as dn
+dnr=dn.array(54)
+print(dnr)
+
+# Create 0-D array with value 55
+import numpy as en
+enr=en.array(55)
+print(enr)
+
+#Create 0-D array with value 56
+import numpy as fn
+fnr=fn.array(56)
+print(fnr)
+
+# create 0-D array with value 57
+import numpy as gn
+gnr=gn.array(57)
+print(gnr)
+
+# Create 0-D array with value 58
+import numpy as hn
+hnr=hn.array(58)
+print(hnr)
+
+# Create 0-D array with value 59
+import numpy as Inn
+inr = Inn.array(59)
+print(inr)
+
+# Create 0-D array with value 50
+import numpy as jn
+jnr=jn.array(50)
+print(jnr)
+
